e7ab9ddb355197699a9791aa8721baf5a72bbac2
intellij <intellij@hotmail.com>
2017-09-05 09:52:57 +0800

    change some thing

--- sample/src/main/java/jp/co/recruit_lifestyle/sample/TestActivity.java
+++ sample/src/main/java/jp/co/recruit_lifestyle/sample/TestActivity.java
@@ -5,6 +5,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 import jp.co.recruit.floatingview.R;
 import jp.co.recruit.floatingview.databinding.ActivityTestBinding;
@@ -14,7 +15,7 @@ import jp.co.recruit.floatingview.databinding.ActivityTestBinding;
  * Created by 201502042 on 2017/9/1.
  */
 
-public class TestActivity extends LifecycleActivity implements TestView{
+public class TestActivity extends LifecycleActivity implements TestView,EventHandler{
 
     private TestPresenter presenter;
     private ActivityTestBinding binding;
@@ -23,12 +24,19 @@ public class TestActivity extends LifecycleActivity implements TestView{
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_test);
         binding.setUser(new User(2,3,"Init set user"));
+        binding.setHandler(this);
         presenter = new TestPresenter(this);
         ViewModelProviders.of(this).get(UserModel.class).getUserLiveData().observe(this, user -> {
             //update UI
             presenter.inflateUser(user);
         });
     }
+
+    @Override
+    public void onVClick(User user) {
+        Toast.makeText(this, ""+user.getName(), Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void updateUserInfo(User user) {
         if (user!=null){
